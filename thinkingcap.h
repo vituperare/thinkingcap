55f628f12c28cadd34ba479979c781c7d598989a
ianmathwiz7@gmail.com <ianmathwiz7@gmail.com@971ba1f6-4d09-4f35-c684-af633274c5ee>
2012-08-27 01:21:54 +0000

    Here is the current thinkingcap library, modified to accommodate Tim's suggestions, and also to make it run more cleanly.

---
+++ thinkingcap.h
@@ -0,0 +1,27 @@
+#ifndef __THINKINGCAP_H__
+#define __THINKINGCAP_H__
+
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+class ThinkingCap {
+  private:
+  int redpin;
+  int greenpin;
+  int bluepin;
+  int currentpin;
+  int magneticpin;
+  int transistorpin;
+  public:
+  ThinkingCap(); //Default Constructor
+  ThinkingCap(int, int, int, int, int, int); //Not-so-default constructor
+  void begin();  //Initializes the Thinking Cap
+  void tDCS(int, int);   //Carries out transcranial Direct Current Stimulation
+  void tMS();    //Carries out transcranial Magnetic Stimulation
+  void CES(int); //Carries out Cranial Electrotherapy Stimulation
+};
+
+#endif
