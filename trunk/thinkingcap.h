76aad39f47c13a312bdba4ee6fa5c5a114b91a97
ianmathwiz7@gmail.com <ianmathwiz7@gmail.com@971ba1f6-4d09-4f35-c684-af633274c5ee>
2012-11-24 03:24:30 +0000

    Added schematics (both the external release and the tMS prototype for internal release) and README file; performed final cleanup; v1.0 is officially ready for release!

--- trunk/thinkingcap.h
+++ trunk/thinkingcap.h
@@ -1,4 +1,13 @@
-#ifndef __THINKINGCAP_H__
+/***************************************************************************
+**Controls an Arduino to send current to a Thinking Cap (v1.0)**
+**Contains methods to initiate tDCS (with ramp-up and ramp-down) and CES**
+**Warning:  Do not exceed 2 mA of current!**
+**This library is designed to work with the corresponding definition file.  If that file
+was not included, you can find it at [insert project URL here]**
+**Written 2012 by ianmathwiz7, of Grindhouse Wetware (ianmathwiz7@gmail.com)**
+***************************************************************************/
+
+ __THINKINGCAP_H__
 #define __THINKINGCAP_H__
 
 #if ARDUINO >= 100
@@ -20,7 +29,7 @@ class ThinkingCap {
   ThinkingCap(int, int, int, int, int, int); //Not-so-default constructor
   void begin();                              //Initializes the Thinking Cap
   void tDCS(int, int);                       //Carries out transcranial Direct Current Stimulation
-  void tMS();                                //Carries out transcranial Magnetic Stimulation
+  //void tMS();                                //Carries out transcranial Magnetic Stimulation
   void CES(int);                             //Carries out Cranial Electrotherapy Stimulation
 };
 
